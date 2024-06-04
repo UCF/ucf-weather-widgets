aea3641fba44241be7061d16e7f5c34e2c643308
Jim Barnes <jimbarnesdeveloper@gmail.com>
2024-06-04 09:40:44 -0400

    Added nice github readme

--- src/edit.js
+++ src/edit.js
@@ -12,7 +12,12 @@ import { __ } from '@wordpress/i18n';
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
-import { Panel, PanelBody, SelectControl, TextControl } from '@wordpress/components';
+import {
+	Panel,
+	PanelBody,
+	SelectControl,
+	TextControl,
+} from '@wordpress/components';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -30,57 +35,68 @@ import './editor.scss';
  *
  * @return {Element} Element to render.
  */
-export default function Edit( { attributes, setAttributes }) {
-	const {
-		longitude,
-		latitude,
-		layout
-	} = attributes;
+export default function Edit( { attributes, setAttributes } ) {
+	const { longitude, latitude, layout } = attributes;
 
 	return (
 		<>
 			<InspectorControls>
 				<Panel>
 					<PanelBody
-						title={ __( 'Map Widget Settings', 'ucf-weather-widgets' ) }
-						icon='admin-plugins'>
+						title={ __(
+							'Map Widget Settings',
+							'ucf-weather-widgets'
+						) }
+						icon="admin-plugins"
+					>
 						<TextControl
 							label={ __( 'Latitude', 'ucf-weather-widgets' ) }
-							help={ __( 'The latitude of the area to display weather for.' ) }
-							onChange={ (latitude) => setAttributes( { latitude } ) }
+							help={ __(
+								'The latitude of the area to display weather for.'
+							) }
+							onChange={ ( latitude ) =>
+								setAttributes( { latitude } )
+							}
 							value={ latitude }
 						/>
 						<TextControl
 							label={ __( 'Longitude', 'ucf-weather-widgets' ) }
-							help={ __( 'The longitude of the area to display weather for.' ) }
-							onChange={ (longitude) => setAttributes( { longitude } ) }
+							help={ __(
+								'The longitude of the area to display weather for.'
+							) }
+							onChange={ ( longitude ) =>
+								setAttributes( { longitude } )
+							}
 							value={ longitude }
 						/>
 						<SelectControl
 							label={ __( 'Layout', 'ucf-weather-widgets' ) }
-							help={ __( 'The layout to use when displaying the widget.' ) }
-							options={
-								[
-									{
-										label: 'Full',
-										value: 'full'
-									},
-									{
-										label: 'Ribbon',
-										value: 'ribbon'
-									},
-									{
-										label: 'Compact',
-										value: 'compact'
-									}
-								]
+							help={ __(
+								'The layout to use when displaying the widget.'
+							) }
+							options={ [
+								{
+									label: 'Full',
+									value: 'full',
+								},
+								{
+									label: 'Ribbon',
+									value: 'ribbon',
+								},
+								{
+									label: 'Compact',
+									value: 'compact',
+								},
+							] }
+							onChange={ ( layout ) =>
+								setAttributes( { layout } )
 							}
-							onChange={ (layout) => setAttributes( {layout} ) }
 							value={ layout }
 						/>
 					</PanelBody>
 				</Panel>
-			</InspectorControls>,
+			</InspectorControls>
+			,
 			<div { ...useBlockProps() }>
 				<p>Latitude: { latitude }</p>
 				<p>Longitude: { longitude }</p>
